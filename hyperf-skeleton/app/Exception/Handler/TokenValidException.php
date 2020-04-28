db6b78dd71b0416aadcf0915a3f63d0c234a4308
kehao1996 <46619057@qq.com>
2020-04-28 12:59:05 +0800

    'a'

--- hyperf-skeleton/app/Exception/Handler/TokenValidException.php
+++ hyperf-skeleton/app/Exception/Handler/TokenValidException.php
@@ -4,31 +4,42 @@ declare(strict_types = 1);
 
 namespace App\Exception\Handler;
 
-use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\ExceptionHandler\ExceptionHandler;
 use Hyperf\HttpMessage\Stream\SwooleStream;
 use Psr\Http\Message\ResponseInterface;
+use App\Exception\FooException;
 use Throwable;
 
+class TokenValidException extends  ExceptionHandler
+{
+    public function handle(Throwable $throwable, ResponseInterface $response)
+    {
+        // 判断被捕获到的异常是希望被捕获的异常
+        if ($throwable instanceof TokenValidException) {
+            // 格式化输出
+            $data = json_encode([
+                'code' => $throwable->getCode(),
+                'message' => $throwable->getMessage(),
+            ], JSON_UNESCAPED_UNICODE);
 
-class TokenValidException extends ExceptionHandler {
+            // 阻止异常冒泡
+            $this->stopPropagation();
+            return $response->withStatus(403)->withBody(new SwooleStream($data));
+        }
+
+        $this->stopPropagation();
+        // 交给下一个异常处理器
+        return $response;
 
 
-    public function handle(Throwable $throwable, ResponseInterface $response) {
-//        $this->stopPropagation();
-//        $result = $this->helper->error(Code::UNAUTHENTICATED, $throwable->getMessage());
-//        return $response->withStatus($throwable->getCode())
-//            ->withAddedHeader('content-type', 'application/json')
-//            ->withBody(new SwooleStream($this->helper->jsonEncode($result)));
-
-
-
-
-        return $response->withHeader("Server", "Hyperf")->withStatus(403)->withBody(new SwooleStream('未登录'));
+        // 或者不做处理直接屏蔽异常
     }
 
-    public function isValid(Throwable $throwable): bool {
-        return $throwable instanceof TokenValidException;
+    /**
+     * 判断该异常处理器是否要对该异常进行处理
+     */
+    public function isValid(Throwable $throwable): bool
+    {
+        return true;
     }
-
 }
