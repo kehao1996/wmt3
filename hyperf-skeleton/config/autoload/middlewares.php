db7cbe6602c56522ba8b02243cdcbc2ab2e667ba
kehao1996 <46619057@qq.com>
2020-04-28 09:41:14 +0800

    '提交'

--- hyperf-skeleton/config/autoload/middlewares.php
+++ hyperf-skeleton/config/autoload/middlewares.php
@@ -14,6 +14,6 @@ return [
     // 这里的 http 对应默认的 server name，如您需要在其它 server 上使用 Session，需要对应的配置全局中间件
     'http' => [
         \Hyperf\Session\Middleware\SessionMiddleware::class,
-        \App\Middleware\CorsMiddleware::class,
+//        \App\Middleware\CorsMiddleware::class,
     ],
 ];
