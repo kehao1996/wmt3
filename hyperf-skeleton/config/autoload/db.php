ed995b00b673c7163ffab1de40f32d9a3d789e70
kehao1996 <46619057@qq.com>
2020-04-28 11:00:46 +0800

    'testhy'

--- hyperf-skeleton/config/autoload/db.php
+++ hyperf-skeleton/config/autoload/db.php
@@ -12,11 +12,11 @@ declare(strict_types=1);
 return [
     'default' => [
         'driver' => 'pdo',
-        'host' => env('DB_HOST', 'localhost'),
+        'host' => 'localhost',
         'port' => env('DB_PORT', 3306),
-        'database' => env('DB_DATABASE', 'hyperf'),
-        'username' => env('DB_USERNAME', 'root'),
-        'password' => env('DB_PASSWORD', ''),
+        'database' => 'wmt',
+        'username' =>  'wmt',
+        'password' => 'wmt123456',
         'charset' => env('DB_CHARSET', 'utf8mb4'),
         'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
         'fetch_mode' => PDO::FETCH_ASSOC,
