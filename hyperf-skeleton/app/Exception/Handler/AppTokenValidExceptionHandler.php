fc1e74245ee220072f9c97bd9fb3cd990d1d8ca0
kehao1996 <46619057@qq.com>
2020-04-28 12:45:00 +0800

    'a'

--- hyperf-skeleton/app/Exception/Handler/AppTokenValidExceptionHandler.php
+++ hyperf-skeleton/app/Exception/Handler/AppTokenValidExceptionHandler.php
@@ -4,29 +4,28 @@ declare(strict_types = 1);
 
 namespace App\Exception\Handler;
 
-use Hyperf\Di\Annotation\Inject;
-use Psr\Http\Message\ResponseInterface;
-use Hyperf\HttpMessage\Stream\SwooleStream;
+use Hyperf\Contract\StdoutLoggerInterface;
 use Hyperf\ExceptionHandler\ExceptionHandler;
-use Phper666\JwtAuth\Exception\TokenValidException;
+use Hyperf\HttpMessage\Stream\SwooleStream;
+use Psr\Http\Message\ResponseInterface;
 use Throwable;
-use App\Helpers\Helper;
-use App\Helpers\Code;
+
 
 class AppTokenValidExceptionHandler extends ExceptionHandler {
 
-    /**
-     * @Inject
-     * @var Helper
-     */
-    protected $helper;
 
     public function handle(Throwable $throwable, ResponseInterface $response) {
-        $this->stopPropagation();
-        $result = $this->helper->error(Code::UNAUTHENTICATED, $throwable->getMessage());
-        return $response->withStatus($throwable->getCode())
-            ->withAddedHeader('content-type', 'application/json')
-            ->withBody(new SwooleStream($this->helper->jsonEncode($result)));
+//        $this->stopPropagation();
+//        $result = $this->helper->error(Code::UNAUTHENTICATED, $throwable->getMessage());
+//        return $response->withStatus($throwable->getCode())
+//            ->withAddedHeader('content-type', 'application/json')
+//            ->withBody(new SwooleStream($this->helper->jsonEncode($result)));
+
+
+        return $response->withHeader("Server", "Hyperf")->withStatus(403)->withBody(new SwooleStream(json_encode([
+            'Status' => 403,
+            'Msg' => '未登录'
+        ])));
     }
 
     public function isValid(Throwable $throwable): bool {
