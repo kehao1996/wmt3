c5a58ce398fa0b6069e2fe7929d1532da89b625b
kehao1996 <46619057@qq.com>
2020-04-29 09:43:25 +0800

    'a'

--- hyperf-skeleton/vendor/phper666/jwt-auth/src/Middleware/JWTAuthMiddleware.php
+++ hyperf-skeleton/vendor/phper666/jwt-auth/src/Middleware/JWTAuthMiddleware.php
@@ -63,7 +63,7 @@ class JWTAuthMiddleware implements MiddlewareInterface
             $response = $response->withHeader('Access-Control-Allow-Origin', '*')
                 ->withHeader('Access-Control-Allow-Credentials', 'true')
                 // Headers 可以根据实际情况进行改写。
-                ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization');
+                ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,authorization');
 
             Context::set(ResponseInterface::class, $response);
             return $handler->handle($request);
