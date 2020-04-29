fa779f3ab5f25c51adf05938a42e56da3b0a62be
kehao1996 <46619057@qq.com>
2020-04-29 09:45:42 +0800

    'a'

--- hyperf-skeleton/vendor/phper666/jwt-auth/src/Middleware/JWTAuthMiddleware.php
+++ hyperf-skeleton/vendor/phper666/jwt-auth/src/Middleware/JWTAuthMiddleware.php
@@ -59,13 +59,6 @@ class JWTAuthMiddleware implements MiddlewareInterface
             }
         }
         if ($isValidToken) {
-            $response = Context::get(ResponseInterface::class);
-            $response = $response->withHeader('Access-Control-Allow-Origin', '*')
-                ->withHeader('Access-Control-Allow-Credentials', 'true')
-                // Headers 可以根据实际情况进行改写。
-                ->withHeader('Access-Control-Allow-Headers', 'DNT,Keep-Alive,User-Agent,Cache-Control,Content-Type,Authorization,authorization');
-
-            Context::set(ResponseInterface::class, $response);
             return $handler->handle($request);
         }
 
